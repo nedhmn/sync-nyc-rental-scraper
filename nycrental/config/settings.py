2ab3c249c6077eee480747d0f6af41a738d57c5b
nedhmn <hermannned@gmail.com>
2025-02-14 03:22:05 -0500

    feat: config

---
+++ nycrental/config/settings.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+import os
+
+from dotenv import load_dotenv
+from pydantic import BaseModel, Field
+from typing import List
+
+load_dotenv()
+
+TIMESTAMP = datetime.now().strftime("%Y%m%d%H%M%S")
+RUN_DIR = f"data/run_{TIMESTAMP}"
+
+
+class Settings(BaseModel):
+    OUTPUT_CSV_FILE: str = Field(f"{RUN_DIR}/results.csv")
+    OUTPUT_LOG_FILE: str = Field(f"{RUN_DIR}/logs.log")
+    BRIGHTDATA_HOST: str = Field(os.getenv("BRIGHTDATA_HOST"))
+    BRIGHTDATA_USERNAME: str = Field(os.getenv("BRIGHTDATA_USERNAME"))
+    BRIGHTDATA_PASSWORD: str = Field(os.getenv("BRIGHTDATA_PASSWORD"))
+    NYC_BASE_URL: str = Field("https://www.nyc.gov")
+    NYC_SOURCE_URL: str = Field("https://www.nyc.gov/site/hpd/services-and-information/sro-buildings-list.page")
+    SE_BASE_URL: str = Field("https://streeteasy.com/search")
+    BOROUGHS_TO_KEEP: List = Field(["MANHATTAN"])
+    NUM_THREADS: int = Field(10)
+    TIMEOUT: int = Field(20)
+
+
+settings = Settings()
